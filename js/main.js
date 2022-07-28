a826a94db0ee19444d3a4a125d7440e4f8ad3e18
marcinj <marcin.jurewicz@gmail.com>
2022-07-28 12:57:37 +0200

    rozwiazanie zadania #39357 js-suma-zmiennych

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 28;
+
+let secondNumber =52;
+
+function sum(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber;
+    
+    return result;
+}
+
+console.log( sum(28, 52) );
+
